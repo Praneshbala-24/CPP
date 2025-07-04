7ac68328a11e310c859a29770ed5d8104d7d57cb
Pranesh Bala <praneshbala24@gmail.com>
2025-07-04 14:44:53 +0530

    Update Singly Linked List q3.cpp

--- Singly Linked List/Singly Linked List q3.cpp
+++ Singly Linked List/Singly Linked List q3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 class Node {
 public:
     int data;
@@ -9,6 +10,7 @@ public:
         next = nullptr;
     }
 };
+
 class SinglyLinkedList {
 private:
     Node* head;
@@ -16,6 +18,7 @@ public:
     SinglyLinkedList() {
         head = nullptr;
     }
+
     void insertAtPosition(int pos, int x) {
         Node* newNode = new Node(x);
         if (pos == 0) {
@@ -23,10 +26,12 @@ public:
             head = newNode;
             return;
         }
+       
         Node* temp = head;
         for (int i = 0; i < pos - 1 && temp != nullptr; ++i) {
             temp = temp->next;
         }
+
         if (temp != nullptr) {
             newNode->next = temp->next;
             temp->next = newNode;
@@ -34,6 +39,7 @@ public:
             cout << "Position out of bounds!" << endl;
         }
     }
+
     void display() {
         Node* temp = head;
         while (temp != nullptr) {
@@ -46,22 +52,27 @@ public:
         cout << endl;
     }
 };
+
 int main() {
     int n, x;
     cin >> n;
     SinglyLinkedList list;
+
     for (int i = 0; i < n; ++i) {
-        int val; cin >> val;
+        int val;
+        cin >> val;
         list.insertAtPosition(i, val);
     }
+
     cin >> x;
-    int middlePos;
-    if (n % 2 == 0) {
-        middlePos = n / 2;
-    } else {
-        middlePos = (n / 2) + 1;
-    }
+    int middlePos ;
+    if(n==1)
+    middlePos = (n+1)/ 2;
+    else
+    middlePos = n / 2;
+    
     list.insertAtPosition(middlePos, x);
     list.display();
+
     return 0;
 }
